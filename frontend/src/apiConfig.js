5a22e9682c44c9314f0163cc00c0ed98d932228b
haetae8554 <03xodbs@gmail.com>
2025-12-16 20:20:52 +0900

    1
    1

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -5,8 +5,8 @@
 const LOCAL_IP = "http://localhost:3001"; 
 
 // 배포시 주소(backend)
-const PROD_URL = "http://192.168.0.101:3000";
+const PROD_URL = "https://ai-closet-stylist.onrender.com";
 
 // 코드에서 사용할 주소
-export const API_BASE_URL = LOCAL_IP;
-// export const API_BASE_URL = PROD_URL;
+//export const API_BASE_URL = LOCAL_IP;
+ export const API_BASE_URL = PROD_URL;
